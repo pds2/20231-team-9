aa800596e0889b53b7d31e2429ab06f33739494d
LucasJunq <lucasjunqueiiraa@gmail.com>
2023-06-09 15:41:21 -0300

    testei algumas coisas

--- src/patrocinador.cpp
+++ src/patrocinador.cpp
@@ -1,4 +1,5 @@
 #include "../include/patrocinador.h"
+#include "mapa.h"
 
 Patrocinador::Patrocinador() {
     
--- tests/test_sistema.cpp
+++ tests/test_sistema.cpp
@@ -1,11 +1,11 @@
-#include "doctest.h"
-#include "sistema.h"
+#include "../third_party/doctest.h"
+#include "../include/sistema.h"
 
 #include <exception>
 
 TEST_CASE("Testa se Sistema cria um personagem") {
     Sistema cobaia;
-    bool v;
+    bool v = false;
     cobaia.criar_participante("Clara",10);
     auto pair = cobaia.get_vivos().begin();
     while(pair != cobaia.get_vivos().end()) {
