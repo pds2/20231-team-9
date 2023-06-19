71190593aa997aee27777f417c2baedba12a7715
FelipeGomide <flgomide@gmail.com>
2023-06-19 12:59:27 -0300

    Atualização das classes bot e jogador

--- src/jogador.cpp
+++ src/jogador.cpp
@@ -8,6 +8,21 @@ using namespace std;
 Jogador::Jogador(string nome, int distrito) : Participante(nome, distrito) { }
 
 void Jogador::definir_acao(){
-    string comando;
+    char comando;
     cin >> comando;
+    switch (comando){
+        case 'a': //atacar
+        
+            break;
+        case 'm': //mover
+            //muda_regiao_atual(destino);
+            break;
+
+        case 'b': //buscar elementos
+            buscar_na_regiao();
+            break;
+        case 'u': //usar utensílio
+            
+            break;
+    }
 }
