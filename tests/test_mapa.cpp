6dd3ecda709bbd324681e81cf7da869da2e08081
LucasJunq <lucasjunqueiiraa@gmail.com>
2023-06-20 10:57:43 -0300

    Acrescentei mais um teste para mapa

--- tests/test_mapa.cpp
+++ tests/test_mapa.cpp
@@ -21,3 +21,25 @@ TEST_CASE("Testa se os personagens são colocados no mapa") {
 
     CHECK(aux == true);
 }
+
+TEST_CASE("Testa se um personagem muda de região corretamente") {
+    Mapa mapa;
+    map<string, Participante*> my_map;
+
+    Jogador Arthur = Jogador("Arthur",10);
+    my_map ["Arthur"] = &Arthur;
+    mapa.inicio(my_map);
+
+    mapa.mover_do_centro(floresta,my_map["Arthur"]);
+
+    //Regiao Floresta(8,7,2,0,regioes::floresta);
+    Regiao R_Arthur = my_map["Arthur"]->get_atual();
+    bool aux = true;
+
+    if(R_Arthur.get_nome() != regioes::floresta) {
+        aux == false;
+    }
+
+    CHECK(aux); 
+
+}
