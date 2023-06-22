96208a96ca172c955a58698405d48977eb81c6d3
Cecília Junqueira <cicijunqueira@hotmail.com>
2023-06-21 22:09:59 -0300

    melhorei o teste de participante, separando os TEST_CASE em vários TEST_CASE

--- tests/test_participante.cpp
+++ tests/test_participante.cpp
@@ -1,20 +1,37 @@
 #include "../third_party/doctest.h"
 #include "../include/jogador.h"
 
-TEST_CASE("Testa se o personagem está na região correta") {
+TEST_CASE("Testa se os atributos de Participante foram corretamente inicializados") {
     Participante *p = new Jogador("Maria", 8);
     CHECK(p->get_atual().get_nome() == regioes::centro);
     CHECK(p->get_distrito() == 8);
     CHECK(p->get_energia() == 100.0);
     CHECK(p->get_hidratacao() == 100.0);
+    delete p;
+}
+
+TEST_CASE("Testa se o personagem está na região correta") {
+    Participante *p = new Jogador("Maria", 8);
 
     Regiao _floresta(8,7,2,0,regioes::floresta);
     p->muda_regiao_atual(_floresta);
     CHECK(p->get_atual().get_nome() == regioes::floresta);
 
+    delete p;
+}
+
+TEST_CASE("Testa se a mudança de arma está ocorrendo corretamente") {
+    Participante *p = new Jogador("Maria", 8);
     Arma _nova_arma = Arma(armas::espada);
     p->adicionar_arma(_nova_arma);
     CHECK(p->get_arma().get_tipo() == armas::espada);
+    delete p;
+}
+
+TEST_CASE("Testa a função batalha") {
+    Participante *p = new Jogador("Maria", 8);
+    Arma _nova_arma = Arma(armas::espada);
+    p->adicionar_arma(_nova_arma);
 
     Participante *p2 = new Jogador("Pedro", 2);
     Arma _nova_arma2 = Arma(armas::faca);
