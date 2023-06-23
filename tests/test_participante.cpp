9e87a1c64ebe27a5a80c721d946464ad927c6fae
Cecília Junqueira <cicijunqueira@hotmail.com>
2023-06-22 21:45:05 -0300

    tentei arrumar o test_participante

--- tests/test_participante.cpp
+++ tests/test_participante.cpp
@@ -3,26 +3,25 @@
 
 TEST_CASE("Testa se os atributos de Participante foram corretamente inicializados") {
     Jogador p = Jogador("Maria", 8);
-    CHECK(p.get_atual().get_nome() == regioes::centro);
-    CHECK(p.get_distrito() == 8);
-    CHECK(p.get_energia() == 100.0);
-    CHECK(p.get_hidratacao() == 100.0);
+    CHECK_EQ(p.get_atual().get_nome(), regioes::centro);
+    CHECK_EQ(p.get_distrito(), 8);
+    CHECK_EQ(p.get_energia(), 100.0);
+    CHECK_EQ(p.get_hidratacao(), 100.0);
     // delete p;
 }
 
 TEST_CASE("Testa se o personagem está na região correta") {
     Jogador p = Jogador("Maria", 8);
-
     Regiao _floresta(8,7,2,0,regioes::floresta);
     p.muda_regiao_atual(_floresta);
-    CHECK(p.get_atual().get_nome() == regioes::floresta);
+    CHECK_EQ(p.get_atual().get_nome(), regioes::floresta);
 }
 
 TEST_CASE("Testa se a mudança de arma está ocorrendo corretamente") {
     Jogador p = Jogador("Maria", 8);
     Arma _nova_arma = Arma(armas::espada);
     p.adicionar_arma(_nova_arma);
-    CHECK(p.get_arma().get_tipo() == armas::espada);
+    CHECK_EQ(p.get_arma().get_tipo(), armas::espada);
 }
 
 TEST_CASE("Testa a função batalha") {
@@ -34,6 +33,6 @@ TEST_CASE("Testa a função batalha") {
     Arma _nova_arma2 = Arma(armas::faca);
     p2.adicionar_arma(_nova_arma2);
     p.batalha(p2);
-    CHECK(p2.get_vivo() == false);
-    CHECK(p.get_energia() == p.get_energia() * p2.get_arma().get_poder() * 0.9);
+    CHECK_EQ(p2.get_vivo(), false);
+    CHECK_EQ(p.get_energia(), p.get_energia() * p2.get_arma().get_poder() * 0.9);
 }
