82495851335fd60f91112ddc496820c9bbabc71e
João Silva <buzelin2@gmail.com>
2023-06-26 03:43:00 -0300

    Comentarios no main.

--- program/main.cpp
+++ program/main.cpp
@@ -4,6 +4,9 @@
 #include "../include/sistema.h"
 
 int main() {
+    /*
+        * @brief Cria o vetor com os participantes
+        */
     vector<Jogador> participantes;
 
     Sistema tudo;
@@ -13,6 +16,10 @@ int main() {
         cout << "Seja Bem vindo ao simulador de jogos vorazes, inicialmente quantos players gostariam de jogar?" \ 
         << endl << "digite um número de 1 a 5" << endl;
         int quantidade = 0;
+        /*
+        * @brief da um check para ver se está valida a quantidade de pessoas.
+        * Sujeito a alteração e adição de mais players jogaveis
+        */
         while(true){
             cin >> quantidade;
             if(quantidade > 5 || quantidade < 1){
@@ -22,6 +29,9 @@ int main() {
                 break;
             }
         }
+        /*
+        * @brief Cria string e int discartaveis e constroi o participante dentro de um vetor de jogadores
+        */
         cout <<"Okay, está na hora de nomear os jogadores" << endl;
         for(int i = 0; i < quantidade; i++){
             string nome;
@@ -32,6 +42,10 @@ int main() {
             cin >> distrito;
             participantes.push_back(Jogador(nome, distrito));
         }
+        /*
+        * @brief Setta a quantidade de bots baseado em uma quantidade total de players em 12
+        * Essa quantidade total de players pode ser alterada
+        */
         cout << "Muito bem, agora você deseja criar os nomes e distritos dos bots, ou deseja que a maquina decida?" \
         << endl<< "Digite 1 para você escolher e 2 para deixar a maquina" << endl;
         int desc = -1;
@@ -44,6 +58,7 @@ int main() {
                 break;
             }
         }
+        
         if(desc == 1){
             for(int i = 0; i < (12 - quantidade); i++){
             string nome;
@@ -54,6 +69,11 @@ int main() {
             cin >> distrito;
             participantes.push_back(Jogador(nome, distrito));
         }
+        /*
+        * @brief Cria nomes aleatorios e alteraveis para os bots
+        * Novamente baseado em 12 pessoas sendo isso alteravél
+        * Depois há de se mudar os bots para o tipo bot
+        */
         if(desc == 2){
             participantes.push_back(Jogador("Jorge", 1));
             participantes.push_back(Jogador("Ana", 1));
